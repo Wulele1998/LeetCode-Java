ef446b6bd73f31406a795f000ccf4dd5b1d4501f
Kevin Wu <Yue.Wu4@ibm.com>
2024-03-06 11:18:09 -0800

    Biweekly Contest 125

--- src/LC3066/Solution.java
+++ src/LC3066/Solution.java
@@ -2,19 +2,27 @@ package LC3066;
 
 import java.util.PriorityQueue;
 
+/**
+ * LC 3066. Minimum Operations to Exceed Threshold Value II
+ */
 public class Solution {
     public int minOperations(int[] nums, int k) {
+        // N: the length of `nums`
+        // time: O(N)
+        // space: O(N)
         PriorityQueue<Long> minHeap = new PriorityQueue<>();
+        int op = 0;
         for (int num : nums) {
             minHeap.add((long) num);
         }
 
-        while (minHeap.size() >= 2) {
-            long num1 = minHeap.poll();
-            long num2 = minHeap.poll();
-
+        while (minHeap.size() >= 2 && minHeap.peek() >= k) {
+            long x = minHeap.poll();
+            long y = minHeap.poll();
+            minHeap.add(Math.min(x, y) * 2 + Math.max(x, y));
+            op++;
         }
 
-        return 0;
+        return op;
     }
 }
