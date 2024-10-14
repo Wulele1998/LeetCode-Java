e256ac18cc1695158668143bd910cd05772cb8b1
Yue Wu <wywade3@163.com>
2024-10-14 10:17:05 -0700

    Daily Practice

--- src/LC1325/Solution.java
+++ src/LC1325/Solution.java
@@ -6,17 +6,21 @@ import library.TreeNode;
  */
 public class Solution {
     public TreeNode removeLeafNodes(TreeNode root, int target) {
+        // N: the number of nodes in the tree
+        // time: O(N)
+        // space: O(N)
         if (root == null) {
             return null;
         }
+
+        // look through the left and right subtree
+        root.left = removeLeafNodes(root.left, target);
+        root.right = removeLeafNodes(root.right, target);
 
         // remove the leave node
         if (root.left == null && root.right == null && root.val == target) {
             return null;
         }
-
-        root.left = removeLeafNodes(root.left, target);
-        root.right = removeLeafNodes(root.right, target);
 
         return root;
     }
