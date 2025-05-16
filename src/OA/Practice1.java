f26b8fbbdfdbeecb41ae2dfae05e51c4f8a88dfc
Wulele1998 <wywade3@gmail.com>
2025-05-15 21:30:48 -0700

    complete the OA question

--- src/OA/Practice1.java
+++ src/OA/Practice1.java
@@ -45,9 +45,37 @@ public class Practice1 {
         }
     }
 
-    private boolean deliverPackage(Package pack) {
-        for (int lockerSize = pack.size; size <= maxSize; size++) {
+    private void handlePackages() {
+        for (Package pack : packages) {
+            if (!deliverPackage(pack)) {
+                System.out.println("Package " + pack.id + " is not delivered");
+            } else {
+                System.out.println("Package " + pack.id + " is delivered to locker " + packageToLocker.get(pack).id);
+            }
+        }
+    }
 
+    private boolean deliverPackage(Package pack) {
+        for (int lockerSize = pack.size; lockerSize <= maxSize; lockerSize++) {
+            if (!lockerMap.get(lockerSize).isEmpty()) {
+                Locker selectedLocker = lockerMap.get(lockerSize).poll();
+                packageToLocker.put(pack, selectedLocker);
+                return true;
+            }
+        }
+        System.out.println("All lockers are used, no available locker for package");
+        return false;
+    }
+
+    private Locker retrievePackage(Package pack) {
+        if (packageToLocker.containsKey(pack)) {
+            Locker locker = packageToLocker.get(pack);
+            lockerMap.get(locker.size).add(locker); // add the locker back to an available queue
+            packageToLocker.remove(pack);
+            return locker;
+        } else {
+            System.out.println("Package " + pack.id + " is not in the Lockers");
+            return null;
         }
     }
 }
